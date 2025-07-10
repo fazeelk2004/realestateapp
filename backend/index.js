2be4588073ae1ff92353537430a89197d645f6d8
Fazeel Khan <fazeelk2004@gmail.com>
2025-07-10 16:16:10 +0500

    connected to the database also created .env file

--- backend/index.js
+++ backend/index.js
@@ -1,4 +1,14 @@
 import express from 'express';
+import mongoose from 'mongoose';
+import dotenv from 'dotenv';
+
+dotenv.config();
+
+mongoose.connect(process.env.MONGO).then(() => {
+  console.log('MongoDB Connected');
+}).catch((err) => {
+  console.error('MongoDB Connection Error:', err);
+});
 
 const app = express();
 
