5fb8f0735bd6d3cb113130585c97dbd29a696dfe
Fazeel Khan <fazeelk2004@gmail.com>
2025-07-12 13:49:42 +0500

    create a middleware and a function to handle possible errors

--- backend/index.js
+++ backend/index.js
@@ -4,22 +4,30 @@ import dotenv from 'dotenv';
 import userRouter from './routes/user.route.js';
 import authRouter from './routes/auth.route.js';
 
-dotenv.config();
+dotenv.config();                                                      // LOADING ENVIRONMENT VARIABLES FROM .env FILE 
 
-mongoose.connect(process.env.MONGO).then(() => {
+mongoose.connect(process.env.MONGO).then(() => {                      // CONNECTING TO MONGODB USING MONGOOSE
   console.log('MongoDB Connected');
 }).catch((err) => {
   console.error('MongoDB Connection Error:', err);
 });
 
-const app = express();
+const app = express();                                                // CREATING AN EXPRESS APPLICATION
 
-app.use(express.json());
+app.use(express.json());                                              //MIDDLEWARE TO PARSE JSON REQUEST BODIES
 
-app.listen(3000, () => {
+app.listen(3000, () => {                                              // STARTING THE SERVER ON PORT 3000
   console.log('Server Is Running On Port 3000');
 });
 
-app.use("/api/user", userRouter);
-
-app.use("/api/auth", authRouter);
+app.use("/api/user", userRouter);                                     // MIDDLEWARE TO HANDLE USER ROUTES
+app.use("/api/auth", authRouter);                                     // MIDDLEWARE TO HANDLE AUTH ROUTES
+app.use((err, req, res, next) => {                                    // MIDDLEWARE TO HANDLE ERRORS
+  const statusCode = err.statusCode || 500;
+  const message = err.message || 'Internal Server Error';
+  return res.status(statusCode).json({
+    success: false,
+    statusCode,
+    message,
+  });
+})
